9bf721e5b042aea2412a2c392059dc1207ca8515
EliasSalom <elias.29@hotmail.com>
2021-06-17 13:39:32 +0300

    Comment

--- js/app.js
+++ js/app.js
@@ -1,32 +1,103 @@
-var question = { "stage_1": { "question1": { "q1": "אני _ שיניים בבוקר ", chose: ["מנקה", "שוטף", "מצחצח", "משמשף"], answer: 3 }, "question2": { "q1": "אני _ ידיים במים וסבון", "a1": "מנקה", "a2": "מצחצח", "a3": "שוטף", "a4": "משמשף", answer: 3 }, "question3": { "q1": "אני _ חולצה קצרה בקיץ", "a1": "שם", "a2": "לובש", "a3": "a3", "a4": "a4", answer: 3 }, "question4": { "q1": "דני _ בים", "a1": "שחה", "a2": "אכל", "a3": "זחל", "a4": "נחה", answer: 3 }, "question5": { "q1": "שרון _ תפוח", "a1": "קפצה", "a2": "כתבה", "a3": "אכלה", "a4": "a4", answer: 3 }, "question6": { "q1": "רן כתב _ לסבא", "a1": "מכתב", "a2": "גלידה", "a3": "סלט", "a4": "a4", answer: 3 } }, "stage_2": { "question7": { image: "/assets/image1.png", chose: ["מנקה", "שוטף", "מצחצח", "משמשף"], answer: 3 }, "question8": { image: "/assets/image2.png", chose: ["מנקה", "שוטף", "מצחצח", "משמשף"], answer: 3 }, "question9": { image: "/assets/image2.png", chose: ["מנקה", "שוטף", "מצחצח", "משמשף"], answer: 3 }, "question10": { image: "/assets/image3.png", chose: ["מנקה", "שוטף", "מצחצח", "משמשף"], answer: 3 }, "question11": { image: "/assets/image4.png", chose: ["מנקה", "שוטף", "מצחצח", "משמשף"], answer: 3 }, "question12": { image: "/assets/image5.png", chose: ["מנקה", "שוטף", "מצחצח", "משמשף"], answer: 3 }, "question13": { image: "/assets/image6.png", chose: ["מנקה", "שוטף", "מצחצח", "משמשף"], answer: 3 } }, "stage_3": { "question14": { images: ["", "", "", ""], ans: ["", "", "", ""], answer: 3 }, "question15": { images: ["", "", "", ""], ans: ["", "", "", ""], answer: 3 }, "question16": { images: ["", "", "", ""], ans: ["", "", "", ""], answer: 3 }, "question17": { images: ["", "", "", ""], ans: ["", "", "", ""], answer: 3 }, "question18": { images: ["", "", "", ""], ans: ["", "", "", ""], answer: 3 }, "question19": { images: ["", "", "", ""], ans: ["", "", "", ""], answer: 3 }, "question20": { images: ["", "", "", ""], ans: ["", "", "", ""], answer: 3 }, "question21": { images: ["", "", "", ""], ans: ["", "", "", ""], answer: 3 }, "question22": { images: ["", "", "", ""], ans: ["", "", "", ""], answer: 3 }, "question23": { images: ["", "", "", ""], ans: ["", "", "", ""], answer: 3 }, "question24": { images: ["", "", "", ""], ans: ["", "", "", ""], answer: 3 }, "question25": { images: ["", "", "", ""], ans: ["", "", "", ""], answer: 3 }, "question26": { images: ["", "", "", ""], ans: ["", "", "", ""], answer: 3 } }, "question27": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question28": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question29": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question30": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question31": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question32": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question33": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question34": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question35": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question36": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question37": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question38": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question39": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question40": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question41": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question42": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question43": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question44": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question45": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question46": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question47": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question48": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question49": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question50": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question51": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question52": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question53": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question54": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question55": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question56": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question57": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question58": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question59": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" }, "question60": { "q1": "How are you", "a1": "a1", "a2": "a2", "a3": "a3", "a4": "a4" } }
+var questions = [
+    {
+      questionText: "אני _ שיניים בבוקר ",
+      chose: ["מנקה", "שוטף", "מצחצח", "משמשף"],
+      answer: 3,
+    },
+    {
+        questionText: "אני _ ידיים במים וסבון",
+      chose: ["שוטף", "משמשף", "מצחצח", "מנקה"],
+      answer: 3,
+    },
+    {
+        questionText: "אני _ חולצה קצרה בקיץ",
+      chose: ["מנקה", "לובש", "מצחצח", "שם"],
+      answer: 3,
+    },
+    {
+      questionText: "אני _ שיניים בבוקר ",
+      chose: ["מנקה", "שוטף", "מצחצח", "משמשף"],
+      answer: 3,
+    },
+    {
+      questionText: "אני _ שיניים בבוקר ",
+      chose: ["מנקה", "שוטף", "מצחצח", "משמשף"],
+      answer: 3,
+    },
+    {
+      questionText: "אני _ שיניים בבוקר ",
+      chose: ["מנקה", "שוטף", "מצחצח", "משמשף"],
+      answer: 3,
+    },
+    {
+        questionText: "אני _ ידיים במים וסבון",
+      chose: ["שוטף", "משמשף", "מצחצח", "מנקה"],
+      answer: 3,
+    },
+    {
+        questionText: "אני _ חולצה קצרה בקיץ",
+      chose: ["מנקה", "לובש", "מצחצח", "שם"],
+      answer: 3,
+    },
+    {
+      questionText: "אני _ שיניים בבוקר ",
+      chose: ["מנקה", "שוטף", "מצחצח", "משמשף"],
+      answer: 3,
+    },
+    {
+      questionText: "אני _ שיניים בבוקר ",
+      chose: ["מנקה", "שוטף", "מצחצח", "משמשף"],
+      answer: 3,
+    },
+
+  ]
 var glob = 1
-var scure = 0
+var img = document.getElementById('img');
+img.style.display = 'none';
+var rightansware=0;
+var ScoreElement = document.getElementById('currScore');
+var btnarry=[4];
+let q = document.getElementById('question');
 
-for(let i =1 ; i<question.length; i++)
+var answer;
+
+function makeQustion() //start problem
 {
-let q = document.getElementById('question').textContent = question.stage_1.question1.q1;
-var image = document.getElementById('img');
-var btn1 = document.getElementById('btn1').textContent = question.stage_1.question1.chose[0];
-var btn2 = document.getElementById('btn2').textContent = question.stage_1.question1.chose[1];
-var btn3 = document.getElementById('btn3').textContent = question.stage_1.question1.chose[2];
-var btn4 = document.getElementById('btn4').textContent = question.stage_1.question1.chose[3];
+        q.textContent = questions[glob].questionText;
+        // q.textContent = question.txt;
+        answer = questions[glob].answer;
+        for(let i=0 ; i < 4 ; i++)
+        {
+            btnarry[i] = document.getElementById('btn'+i);
+            btnarry[i].textContent = questions[glob].chose[i];
+        }
+    
+}   
 
+function setImageVisible(id, visible) {
+    var img = document.getElementById(id);
+    img.style.display = (visible ? 'block' : 'none');
+}
 
-var unvisible = document.getElementById('correctAns') = question['question' + a].answer;
-
+function AddScore()
+{
+    rightansware++;
+    ScoreElement.textContent=rightansware;
+    glob++;
+}
 
 //Check button
-function checkans(bynid) {
-    if (btnid === 'btn' + unvisible) {
-        document.getElementById(btnid).style.backgroundColor = 'green';
-        scure++;
-        glob++;
-        document.getElementById('currScore').innerHTML = scure;
-        // ques()
+function checkans(btnid) {
+    if (btnid.id === 'btn' + answer) {
+        document.getElementById(btnid.id).style.backgroundColor = 'green';
+        AddScore()
+        makeQustion();
     }
     else {
-        document.getElementById(btnid).style.backgroundColor = 'red';
-        document.getElementById(unvisible).style.backgroundColor = 'red';
+        document.getElementById(btnid.id).style.backgroundColor = 'red';
+        document.getElementById('btn'+answer).style.backgroundColor = 'green';
+        AddScore()
+        makeQustion();
     }
 }
 
@@ -52,4 +123,5 @@ window.onload = function () {
     var oneMinute = 60,
         display = document.querySelector('.time');
     startTimer(oneMinute, display);
+    makeQustion();
 };
